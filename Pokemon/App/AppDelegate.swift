82c6cab7709741e1f4ce018de2e09369741fe4dc
MaximLaskin <maxlaskin@mail.ru>
2023-03-10 23:07:12 +0300

    Initial Commit

---
+++ Pokemon/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Pokemon
+//
+//  Created by Maxim on 10.03.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        return true
+    }
+}
